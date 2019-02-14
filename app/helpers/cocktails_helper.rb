7ff9332a2e35b90a670363e552286a13d3299842
Quentin Riviere <quentin.l.riviere@gmail.com>
2019-02-14 11:29:38 +0100

    done

--- app/helpers/cocktails_helper.rb
+++ app/helpers/cocktails_helper.rb
@@ -1,2 +1,9 @@
 module CocktailsHelper
+  def photo_url_for(cocktail)
+      if cocktail.photo.present?
+        cl_image_path cocktail.photo, height: 300, width: 400, crop: :fill
+      else
+        'http://unsplash.it/360/175'
+      end
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+Ingredient.destroy.all? { |e|  }
 
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
+
+url = "san_francisco.jpg"
+Cocktail.new(name: 'NES', photo: "#{url}")
